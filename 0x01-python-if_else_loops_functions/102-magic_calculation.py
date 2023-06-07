07b242ec8876543244678f3c72cc8b37678e575c
Prosper Emebo <emeboprosper@gmail.com>
2023-06-07 09:03:46 +0100

    Create 102-magic_calculation.py

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Python bytecode clone
+
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return (a * b) - c
