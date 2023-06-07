673c59d6e0e47d16af1b7a3c7a10f4266d033f74
Prosper Emebo <emeboprosper@gmail.com>
2023-06-07 03:37:31 +0100

    New Push

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-# This script generates a random integer,
-# and checks if it's positive, negative or zero.
+# This script generates a random number,
+# and prints positive, negative or zero.
 
 import random
 number = random.randint(-10, 10)
-if number < 0:
-	print(f"{number:d} is negative")
-elif number == 0:
-	print(f"{number:d} is zero")
+if number > 0:
+    print(f'{number:d} is positive')
+elif number < 0:
+    print(f'{number:d} is negative')
 else:
-	print(f"{number:d} is positive")
+    print(f'{number:d} is zero')
