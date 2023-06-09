2d184aa3d4b8354aa613f5f10c0099ea7cdcd9f5
Prosper Emebo <emeboprosper@gmail.com>
2023-06-09 03:52:07 +0100

    Push

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# This is script prints the length and the 
+# list of its arguments.
+
+from sys import argv
+if __name__ == "__main__":
+    print("{} {}{}{}".format(len(argv) - 1,
+          "argument", "s" if len(argv) - 1 > 1 else "",
+                             ":" if len(argv) - 1 > 0 else "."))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
