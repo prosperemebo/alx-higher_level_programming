5b6f9ed7750652fd2f4a35ee6290e7bcf9aab549
Prosper Emebo <emeboprosper@gmail.com>
2023-06-09 03:44:34 +0100

    New Push

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-# This script uses a module to perform
-# an addition task
+# This script imports the function add(a, b)
+# from a module and outputs the result of the addition 1 + 2 = 3
 
-from add_0 import add_0
-if __name__ == "__main__"
+from add_0 import add
+if __name__ == "__main__":
     a = 1
     b = 2
-    print("{} + {}".format((a, b, add(a, b))))
+    print("{} + {} = {}".format(a, b, add(a, b)))
