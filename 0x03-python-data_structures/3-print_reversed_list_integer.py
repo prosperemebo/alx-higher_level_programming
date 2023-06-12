9e9b96f67fa916aab87c0743ec40b18effaf3e88
Prosper Emebo <emeboprosper@gmail.com>
2023-06-12 16:32:45 +0100

    New Push

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,6 @@
 # in input list in reverse
 
 def print_reversed_list_integer(my_list=[]):
-	if my_list:
-        	for num in range(len(my_list) - 1, -1, -1):
-            		print("{:d}".format(my_list[num]))
+    if my_list:
+        for num in range(len(my_list) - 1, -1, -1):
+            print("{:d}".format(my_list[num]))
