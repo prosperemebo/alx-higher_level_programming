befbca19bb0df60a6a4832d5df34017a31164364
Prosper Emebo <emeboprosper@gmail.com>
2023-06-12 16:24:24 +0100

    New Push

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-# This is a function that retrieves an element from a list like in C.
+# Retireves item from list securely
 
 def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return None
-    return my_list[idx]
+	if (idx < 0 or idx >= len(my_list)):
+		return None
+	else: 
+		return my_list[idx]
