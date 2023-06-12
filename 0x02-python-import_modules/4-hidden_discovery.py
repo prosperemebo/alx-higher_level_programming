7c400a1c6d06305ca198b77884bac6fe299cfa03
Prosper Emebo <emeboprosper@gmail.com>
2023-06-12 15:54:36 +0100

    New Push

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# This is a script that prints the names
+# defined by the compiled module hidden_4.pyc .
+
+if __name__ == "__main__":
+    import hidden_4
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,3 @@
+# 0x03-python-data_structures
+
+Python - Data Structures: Lists, Tuples
