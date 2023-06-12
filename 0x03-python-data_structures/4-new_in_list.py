5deac625b20ecdb2ef86ff538fd87eef997533a9
Prosper Emebo <emeboprosper@gmail.com>
2023-06-12 17:23:46 +0100

    New Push

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Replaces element in input list securely
+
+def new_in_list(my_list, idx, element):
+    new_list = []
+
+    for i in range(len(my_list)):
+        new_list.append(element if i == idx else my_list[i])
+    return new_list
