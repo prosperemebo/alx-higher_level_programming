649ba9f4d28062a446e775888dcc6b5febe2639f
Prosper Emebo <emeboprosper@gmail.com>
2023-06-12 17:20:55 +0100

    New Push

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-# Retireves item from list securely
+# Retrieves item from list securely
 
 def element_at(my_list, idx):
     if (idx < 0 or idx >= len(my_list)):
         return None
-    else: 
+    else:
         return my_list[idx]
