0e09db2f71f1f13e18aae11c1abe7649341044af
Prosper Emebo <emeboprosper@gmail.com>
2023-06-14 01:01:58 +0100

    New Push

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,9 @@
 
 from sys import argv
 if __name__ == "__main__":
-    print("{} {}{}{}".format(len(argv) - 1,
-        "argument", "s" if len(argv) - 1 != 1 else "",
-        ":" if len(argv) - 1 > 0 else "."))
+    print("{} {}{}{}".format(len(argv) - 1, 
+                             "argument", 
+                             "s" if len(argv) - 1 != 1 else "", 
+                             ":" if len(argv) - 1 > 0 else "."))
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
