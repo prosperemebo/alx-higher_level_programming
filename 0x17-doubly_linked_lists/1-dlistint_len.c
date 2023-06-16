98afa953a0da4e8a072cae33785b1f7fc0f6adf7
Prosper Emebo <emeboprosper@gmail.com>
2023-06-16 04:52:01 +0100

    New Push

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all the elements of a dlistint_t list
+ * dlistint_len - Counts the number of node in input list
  *
  * @h: Pointer to the head of the list.
  *
- * Return: The number of nodes in the list.
+ * Return: The number of nodes in the list
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *current = h;
 	size_t count = 0;
 
 	while (current != NULL)
 	{
-		printf("%d\n", current->n);
 		current = current->next;
 		count++;
 	}
