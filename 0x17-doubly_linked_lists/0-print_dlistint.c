384ad99bfc1b3373bbc74173776b545f524e7cd7
Prosper Emebo <emeboprosper@gmail.com>
2023-06-16 04:47:26 +0100

    new Push

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		count++;
+	}
+
+	return count;
+}
