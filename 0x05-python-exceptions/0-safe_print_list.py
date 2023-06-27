942107f4313d076bdefa2b2a3448efd27620ed53
Prosper Emebo <emeboprosper@gmail.com>
2023-06-27 02:17:48 +0100

    First Push

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Saftely prints all elements
+# in Input list
+
+def safe_print_list(my_list=[], x=0):
+    num = 0
+    for i in range(x):
+        try: 
+            print(my_list[i], end='')
+            num += 1
+        except IndexError:
+            break
+    print()
+    return num
