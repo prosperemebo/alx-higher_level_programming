4ffa2c4662a7453b35d82bdf31f487edfb4a9a61
Prosper Emebo <emeboprosper@gmail.com>
2023-06-27 02:39:37 +0100

    Update 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
     len = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i], end=''))
+            print("{:d}".format(my_list[i]), end='')
             len += 1
         except (TypeError, ValueError):
             continue
