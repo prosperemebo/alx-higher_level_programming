78d4977392fc6c27467d36311ffb5849a10b1121
Prosper Emebo <emeboprosper@gmail.com>
2023-06-27 02:46:54 +0100

    New Push

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # prints the first x elements
 # of a list and only integers.
-
 def safe_print_list_integers(my_list=[], x=0):
     len = 0
     for i in range(x):
@@ -12,4 +11,3 @@ def safe_print_list_integers(my_list=[], x=0):
             continue
     print('')
     return len
-
---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# divides 2 integers and
+# prints the result.
+
+def safe_print_division(a, b):
+    ans = 0
+    try:
+        ans = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result {}".format(ans))
+    return ans
