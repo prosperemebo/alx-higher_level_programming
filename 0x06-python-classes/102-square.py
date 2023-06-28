76a7e9d06a4cee9bd5d0dd47cddc985192355b27
Prosper Emebo <emeboprosper@gmail.com>
2023-06-28 23:02:46 +0100

    New Push

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+This module creates a class Square.
+"""
+
+
+class Square:
+    """
+    An empty class named Square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new private instance of the Square class.
+        And raises TypeError and ValueError
+
+        Args:
+            size (int): Size of square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Calculates area of square.
+        """
+        return self.size ** 2
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
