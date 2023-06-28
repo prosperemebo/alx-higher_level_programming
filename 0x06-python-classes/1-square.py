1460ccf498c4f225404012afa27758ad41272d73
Prosper Emebo <emeboprosper@gmail.com>
2023-06-28 22:50:01 +0100

    New Push

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This script creates a class Square
+"""
+
+
+class Square:
+    """
+    A class named Square.
+
+    Args:
+        size (int): size of square
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new private instance of the Square class
+
+        Args:
+            _Square__size (int): Size of squae
+        """
+        self._Square__size = size
