31baf373a2d3a1e4ab961c84b84a412a60384736
Prosper Emebo <emeboprosper@gmail.com>
2023-07-03 22:56:50 +0100

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -51,19 +51,19 @@ def print_solutions(solutions):
 
 
 def main():
-    if len(sys.argv) != 2:
+    if len(argv) != 2:
         print("Usage: nqueens N")
-        sys.exit(1)
+        exit(1)
 
     try:
-        N = int(sys.argv[1])
+        N = int(argv[1])
     except ValueError:
         print("N must be a number")
-        sys.exit(1)
+        exit(1)
 
     if N < 4:
         print("N must be at least 4")
-        sys.exit(1)
+        exit(1)
 
     board = [[0] * N for _ in range(N)]
     solutions = []
