8c83baace6d33b8da332ed44bfc27fcc0b8f741b
Prosper Emebo <emeboprosper@gmail.com>
2023-07-05 02:58:05 +0100

    Create 101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for class LockedClass."""
+
+
+class LockedClass:
+    """
+    class LockedClass
+    Restricts attributs to first_name.
+    """
+    __slots__ = ["first_name"]
