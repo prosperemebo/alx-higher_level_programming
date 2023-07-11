4bc7e72e943f9128983a4c707b6dd1906b49f8ef
Prosper Emebo <emeboprosper@gmail.com>
2023-07-11 05:35:56 +0100

    New Push

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module for the function add_attribute."""
+
+
+def add_attribute(obj, attr, value):
+    """Add a new attribute to an object if it’s possible.
+
+    Args:
+        obj: object
+        attr: attribute
+        value: attribute value
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attr, value)
