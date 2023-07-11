ea9a908b9b5b68fd1f20ddd9326445e5d0e505f7
Prosper Emebo <emeboprosper@gmail.com>
2023-07-11 02:36:53 +0100

    New Push

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""BaseGeometry Class."""
+
+
+class BaseGeometry:
+    """BaseGeometry class."""
+
+    def area(self):
+        """Raise exception.
+
+        Raises:
+            Exception: _description_
+        """        
+        raise Exception("area() is not implemented")
