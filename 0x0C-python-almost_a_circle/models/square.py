4c9537fd04a79bbeef26c8019c364f197e83693a
prosperemebo <emeboprosper@gmail.com>
2023-07-17 23:02:22 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -60,3 +60,17 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        Return the dictionary representation of a Square instance.
+
+        Returns:
+            dict: A dictionary representation of the Square instance.
+        """
+        return {
+            'id': self.id,
+            'size': self.width,
+            'x': self.x,
+            'y': self.y
+        }
