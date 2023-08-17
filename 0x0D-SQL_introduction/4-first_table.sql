178517138ffd3acc51e364240b0e04689e03fef8
prosperemebo <emeboprosper@gmail.com>
2023-08-17 03:25:16 +0100

    New push

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- This script creates a table called first_table in the current database of your MySQL server.
+CREATE TABLE IF NOT EXISTS FIRST_TABLE (
+	ID INTEGER,
+	NAME VARCHAR(256)
+);
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,2 @@
+## 0x0E-SQL_more_queries
+SQL - The deep dive.
