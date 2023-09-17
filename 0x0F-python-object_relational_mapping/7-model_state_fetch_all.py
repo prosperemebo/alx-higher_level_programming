a591f342b47a69a73976fbbfdf5380ec93344aef
prosperemebo <emeboprosper@gmail.com>
2023-09-17 12:53:49 +0100

    New Push

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,8 +7,13 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine(f"mysql://{argv[1]}:{argv[2]}@\
-localhost:3306/{argv[3]}")
+    db_name = argv[3]
+    db_user = argv[1]
+    db_password = argv[2]
+    uri = "mysql://{}:{}@\
+localhost:3306/{}".format(db_user, db_password, db_name)
+
+    engine = create_engine(uri)
     Session = sessionmaker(bind=engine)
     session = Session()
     res = session.query(State).all()
