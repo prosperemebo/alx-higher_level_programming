d7c46dbd6857046aa75eff5dc946f686afbe1992
prosperemebo <emeboprosper@gmail.com>
2023-09-17 10:09:21 +0100

    Create 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This script lists all states from the database hbtn_0e_0_usa."""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db_name = argv[3]
+    db_user = argv[1]
+    db_password = argv[2]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=db_user,
+        password=db_password,
+        database=db_name,
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    result = cursor.fetchall()
+
+    for row in result:
+        print(row)
+    cursor.close()
+    db.close()
