d4c542300762973b27f635d6c888d45880b0e90a
prosperemebo <emeboprosper@gmail.com>
2023-09-17 14:45:55 +0100

    Create 10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This script prints the State object with the name passed as argument.
+"""
+from sqlalchemy import create_engine
+from model_state import State
+from sqlalchemy.orm import sessionmaker
+
+from sys import argv
+
+if __name__ == "__main__":
+    db_name = argv[3]
+    db_user = argv[1]
+    db_password = argv[2]
+    uri = "mysql://{}:{}@\
+localhost:3306/{}".format(db_user, db_password, db_name)
+
+    engine = create_engine(uri)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_name = argv[4]
+    res = session.query(State).filter(State.name.like(state_name)).first()
+    if res:
+        print("{}".format(res.id))
+    else:
+        print("Not found")
+    session.close()
