96e1c6639d8c80a5e214f7d486c3305a89f6eb72
prosperemebo <emeboprosper@gmail.com>
2023-09-17 12:54:33 +0100

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""This script lists all State objects from the database hbtn_0e_6_usa."""
+"""
+This script lists all State objects.
+
+from the database hbtn_0e_6_usa.
+"""
 from sqlalchemy import create_engine
 from model_state import State
 from sqlalchemy.orm import sessionmaker
