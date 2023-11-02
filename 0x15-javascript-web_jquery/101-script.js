616409c23ba84e3f81aada1f2c05c388d9f57737
prosperemebo <emeboprosper@gmail.com>
2023-11-02 01:01:48 +0100

    Update 101-script.js

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,23 +1,27 @@
+function addItemHandler() {
+  const newItem = $('<li>Item</li>');
+  $('ul.my_list').append(newItem);
+}
+
+function removeItemHandler() {
+  const list = $('ul.my_list');
+  if (list.children('li').length > 0) {
+    list.children('li:last').remove();
+  }
+}
+
+function clearListHandler() {
+  $('ul.my_list').empty();
+}
+
 function main() {
   const addItem = $('#add_item');
   const removeItem = $('#remove_item');
   const clearList = $('#clear_list');
 
-  addItem.click(function () {
-    const newItem = $('<li>Item</li>');
-    $('ul.my_list').append(newItem);
-  });
-
-  removeItem.click(function () {
-    const list = $('ul.my_list');
-    if (list.children('li').length > 0) {
-      list.children('li:last').remove();
-    }
-  });
-
-  clearList.click(function () {
-    $('ul.my_list').empty();
-  });
+  addItem.click(addItemHandler);
+  removeItem.click(removeItemHandler);
+  clearList.click(clearListHandler);
 }
 
 $(document).ready(main);
